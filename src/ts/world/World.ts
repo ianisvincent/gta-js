afa33c407a203c68fab68432e59f7f76dbcd6e87
ianisvincent <ianis@hetchr.com>
2021-06-11 07:26:15 +0200

    Add new vertical camera offset, and new camera distance from character - Add camera helper

--- src/ts/world/World.ts
+++ src/ts/world/World.ts
@@ -107,8 +107,9 @@ export class World
 
 		// Three.js scene
 		this.graphicsWorld = new THREE.Scene();
-		this.camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1010);
-
+		this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1010);
+		const helper = new THREE.CameraHelper(this.camera);
+		this.graphicsWorld.add(helper);
 		// Passes
 		let renderPass = new RenderPass( this.graphicsWorld, this.camera );
 		let fxaaPass = new ShaderPass( FXAAShader );
