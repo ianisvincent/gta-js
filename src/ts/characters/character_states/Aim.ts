b93bf742a8ad6bc461c068e3d644cd428548c8e1
ianisvincent <ianis@hetchr.com>
2021-07-01 08:28:18 +0200

    Add gun recoil force on shoot

--- src/ts/characters/character_states/Aim.ts
+++ src/ts/characters/character_states/Aim.ts
@@ -4,11 +4,14 @@ import { Character } from "../Character";
 import { Idle } from "./Idle";
 import * as THREE from 'three';
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { BodyPart } from "../../enums/BodyPart";
 
 export class Aim extends CharacterStateBase implements ICharacterState {
     public rayCaster: THREE.Raycaster;
     private intersectedObject: any;
     private dummySphereImpact: any;
+    private shootingCount = 0;
+    private readonly recoilForce = 0.008;
 
     constructor(character: Character) {
         super(character);
@@ -25,22 +28,46 @@ export class Aim extends CharacterStateBase implements ICharacterState {
         this.fallInAir();
         this.spawnObjectOnPoint();
         if (this.character.cameraOperator) {
-            this.rayCaster.setFromCamera(new THREE.Vector2(), this.character.cameraOperator.camera)
+            this.rayCaster.setFromCamera(new THREE.Vector2(0.05, 0), this.character.cameraOperator.camera)
             const intersects = this.rayCaster.intersectObjects(this.character.world.graphicsWorld.children, true);
             for (let i = 0; i < intersects.length; i++) {
                 if (intersects[i].object.type !== 'LineSegments' && this.dummySphereImpact) {
                     this.intersectedObject = intersects[0];
                 }
-        }
-        // If this character stop aiming, go back to idle state
-        if (this.animationEnded(timeStep)) {
-            this.character.setState(new Idle(this.character))
-        }
+            }
+            // If this character stop aiming, go back to idle state
+            if (this.animationEnded(timeStep)) {
+                this.character.setState(new Idle(this.character))
+            }
 
-        if (this.character.actions.shoot.isPressed) {
-            this.spawnImpactOnPoint(this.intersectedObject);
+            if (this.character.actions.shoot.isPressed) {
+                console.log(this.shootingCount);
+                this.shootingCount += 1;
+                if (this.shootingCount === 1) {
+                    console.log('shoitt!');
+                    this.spawnImpactOnPoint(this.intersectedObject);
+                    this.setGunRecoilToForeArms();
+                }
+            }
+
         }
     }
+
+    public setGunRecoilToForeArms(): void {
+        const rightForeArm = this.character.getObjectByName(BodyPart.RightForeArm);
+        const leftForeArm = this.character.getObjectByName(BodyPart.LeftForeArm);
+        leftForeArm.rotation.y -= this.recoilForce;
+        rightForeArm.rotation.y += this.recoilForce;
+        rightForeArm.rotation.z -= this.recoilForce;
+    }
+
+
+    public restoreInitialForeArmsPosition(): void {
+        const rightForeArm = this.character.getObjectByName(BodyPart.RightForeArm);
+        const leftForeArm = this.character.getObjectByName(BodyPart.LeftForeArm);
+        leftForeArm.rotation.y += this.recoilForce;
+        rightForeArm.rotation.y -= this.recoilForce;
+        rightForeArm.rotation.z += this.recoilForce;
     }
 
     public onInputChange(): void {
@@ -48,6 +75,11 @@ export class Aim extends CharacterStateBase implements ICharacterState {
 
         if (this.character.actions.aim.justReleased) {
             this.character.setState(new Idle(this.character))
+        }
+
+        if (this.character.actions.shoot.justReleased) {
+            this.shootingCount = 0;
+            this.restoreInitialForeArmsPosition();
         }
     }
 
