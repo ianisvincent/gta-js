70973b4145cbf1907324b1f6715a277c0e8f8edb
ianisvincent <ianis@hetchr.com>
2021-07-01 08:28:18 +0200

    Add gun recoil force on shoot

--- src/ts/enums/BodyPart.ts
+++ src/ts/enums/BodyPart.ts
@@ -1,4 +1,6 @@
 export enum BodyPart {
     LeftHand = 'mixamorig12LeftHand',
-    RightHand = 'mixamorig12RightHand'
+    RightHand = 'mixamorig12RightHand',
+    LeftForeArm = 'mixamorig12LeftForeArm',
+    RightForeArm = 'mixamorig12RightForeArm'
 }
