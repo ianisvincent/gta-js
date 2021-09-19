1864ffd486a32ddd5694760668bad97198416e9a
ianisvincent <ianis@hetchr.com>
2021-09-19 18:34:24 +0200

    Add ScaredRun and init WalkPath on Npc class

--- src/ts/characters/npc_states/ScaredRun.ts
+++ src/ts/characters/npc_states/ScaredRun.ts
@@ -1,19 +1,18 @@
-import { Character } from "../Character";
+import { ICharacterState } from '../../interfaces/ICharacterState';
+import { Character } from '../Character';
 import { NpcStateBase } from "./NpcStateBase";
-import { ICharacterState } from "../../interfaces/ICharacterState";
-import { ScaredRun } from "./ScaredRun";
 
-export class Scared extends NpcStateBase implements ICharacterState {
+export class ScaredRun extends NpcStateBase implements ICharacterState {
     constructor(character: Character) {
         super(character);
         this.character.velocitySimulator.damping = 0.6;
         this.character.velocitySimulator.mass = 10;
         this.character.setArcadeVelocityTarget(0);
-        this.playAnimation('ducking', 0.1, true, true);
-        setTimeout(() => {
-            this.character.setState(new ScaredRun(this.character));
-        }, 1000);
-
+        this.playAnimation('scared_run', 0.1, false, false);
+        this.character.velocitySimulator.mass = 10;
+        this.character.rotationSimulator.damping = 0.8;
+        this.character.rotationSimulator.mass = 50;
+        this.character.setArcadeVelocityTarget(1.1);
     }
 
     public update(timeStep: number): void {
