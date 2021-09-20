62547f403be9e46d70ce24f2c6d2b9182f94a02f
ianisvincent <ianis@hetchr.com>
2021-09-20 09:07:19 +0200

    Add ScaredRun a second after Scared for NPCs

--- src/ts/characters/character_states/Scared.ts
+++ src/ts/characters/character_states/Scared.ts
@@ -1,15 +1,22 @@
-import { Character } from "../Character";
 import { ICharacterState } from "../../interfaces/ICharacterState";
 import { NpcStateBase } from "./NpcStateBase";
+import { Npc } from "../Npc";
+import { ScaredRun } from "./ScaredRun";
 
 export class Scared extends NpcStateBase implements ICharacterState {
-    constructor(character: Character) {
-        super(character);
+    npc: Npc;
+
+    constructor(npc: Npc) {
+        super(npc);
+        this.npc = npc;
         this.character.velocitySimulator.damping = 0.6;
         this.character.velocitySimulator.mass = 10;
         this.character.setArcadeVelocityTarget(0);
         this.playAnimation('ducking', 0.1, true, true);
-    }
+        setTimeout(() => {
+            this.character.setState(new ScaredRun(this.character));
+        }, 1000);
+    }z
 
     public update(timeStep: number): void {
         super.update(timeStep);
