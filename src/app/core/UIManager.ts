55582e4d5e821d57ff504cf899710a738b06a1a9
ianisvincent <ianis@hetchr.com>
2022-01-23 09:49:01 +0100

    [Refactoring] Format LoadingManager and UIManager

--- src/app/core/UIManager.ts
+++ src/app/core/UIManager.ts
@@ -1,7 +1,5 @@
-export class UIManager
-{
-	public static setLoadingScreenVisible(value: boolean): void
-	{
-		document.getElementById('loading-screen').style.display = value ? 'flex' : 'none';
-	}
+export class UIManager {
+    public static setLoadingScreenVisible(value: boolean): void {
+        document.getElementById('loading-screen').style.display = value ? 'flex' : 'none';
+    }
 }
