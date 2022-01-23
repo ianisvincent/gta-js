9627b6b90d34c559e96326e3794ed5f805651909
ianisvincent <ianis@hetchr.com>
2022-01-23 09:49:01 +0100

    [Refactoring] Format LoadingManager and UIManager

--- src/app/core/LoadingManager.ts
+++ src/app/core/LoadingManager.ts
@@ -5,115 +5,98 @@ import { Scenario } from '../world/Scenario';
 import Swal from 'sweetalert2';
 import { World } from '../world/World';
 
-export class LoadingManager
-{
-	public firstLoad: boolean = true;
-	public onFinishedCallback: () => void;
+export class LoadingManager {
+    public firstLoad = true;
+    public onFinishedCallback: () => void;
 
-	private world: World;
-	private gltfLoader: GLTFLoader;
-	private loadingTracker: LoadingTrackerEntry[] = [];
+    private world: World;
+    private gltfLoader: GLTFLoader;
+    private loadingTracker: LoadingTrackerEntry[] = [];
 
-	constructor(world: World)
-	{
-		this.world = world;
-		this.gltfLoader = new GLTFLoader();
+    constructor(world: World) {
+        this.world = world;
+        this.gltfLoader = new GLTFLoader();
 
-		this.world.setTimeScale(0);
-		UIManager.setLoadingScreenVisible(true);
-	}
+        this.world.setTimeScale(0);
+        UIManager.setLoadingScreenVisible(true);
+    }
 
-	public loadGLTF(path: string, onLoadingFinished: (gltf: any) => void): void
-	{
-	  console.log(path);
-		let trackerEntry = this.addLoadingEntry(path);
+    public loadGLTF(path: string, onLoadingFinished: (gltf: any) => void): void {
+        console.log(path);
+        const trackerEntry = this.addLoadingEntry(path);
 
-		this.gltfLoader.load(path,
-		(gltf)  =>
-		{
-			onLoadingFinished(gltf);
-			this.doneLoading(trackerEntry);
-		},
-		(xhr) =>
-		{
-			if ( xhr.lengthComputable )
-			{
-				trackerEntry.progress = xhr.loaded / xhr.total;
-			}
-		},
-		(error)  =>
-		{
-			console.log('failed to load', path);
-			console.error(error);
-		});
-	}
+        this.gltfLoader.load(path,
+            (gltf) => {
+                onLoadingFinished(gltf);
+                this.doneLoading(trackerEntry);
+            },
+            (xhr) => {
+                if (xhr.lengthComputable) {
+                    trackerEntry.progress = xhr.loaded / xhr.total;
+                }
+            },
+            (error) => {
+                console.log('failed to load', path);
+                console.error(error);
+            });
+    }
 
-	public addLoadingEntry(path: string): LoadingTrackerEntry
-	{
-		let entry = new LoadingTrackerEntry(path);
-		this.loadingTracker.push(entry);
+    public addLoadingEntry(path: string): LoadingTrackerEntry {
+        const entry = new LoadingTrackerEntry(path);
+        this.loadingTracker.push(entry);
 
-		return entry;
-	}
+        return entry;
+    }
 
-	public doneLoading(trackerEntry: LoadingTrackerEntry): void
-	{
-		trackerEntry.finished = true;
-		trackerEntry.progress = 1;
+    public doneLoading(trackerEntry: LoadingTrackerEntry): void {
+        trackerEntry.finished = true;
+        trackerEntry.progress = 1;
 
-		if (this.isLoadingDone())
-		{
-			if (this.onFinishedCallback !== undefined)
-			{
-				this.onFinishedCallback();
-			}
+        if (this.isLoadingDone()) {
+            if (this.onFinishedCallback !== undefined) {
+                this.onFinishedCallback();
+            }
 
-			UIManager.setLoadingScreenVisible(false);
-		}
-	}
+            UIManager.setLoadingScreenVisible(false);
+        }
+    }
 
-	public createWelcomeScreenCallback(scenario: Scenario): void
-	{
-		if (this.onFinishedCallback === undefined)
-		{
-			this.onFinishedCallback = () =>
-			{
-				this.world.update(1, 1);
+    public createWelcomeScreenCallback(scenario: Scenario): void {
+        if (this.onFinishedCallback === undefined) {
+            this.onFinishedCallback = () => {
+                this.world.update(1, 1);
 
-				Swal.fire({
-					title: scenario.descriptionTitle,
-					html: scenario.descriptionContent,
-					confirmButtonText: 'Play',
-					buttonsStyling: false,
-					onClose: () => {
-						this.world.setTimeScale(1);
-					}
-				});
-			};
-		}
-	}
+                Swal.fire({
+                    title: scenario.descriptionTitle,
+                    html: scenario.descriptionContent,
+                    confirmButtonText: 'Play',
+                    buttonsStyling: false,
+                    onClose: () => {
+                        this.world.setTimeScale(1);
+                    }
+                });
+            };
+        }
+    }
 
-	private getLoadingPercentage(): number
-	{
-		let done = true;
-		let total = 0;
-		let finished = 0;
+    private getLoadingPercentage(): number {
+        let done = true;
+        let total = 0;
+        let finished = 0;
 
-		for (const item of this.loadingTracker)
-		{
-			total++;
-			finished += item.progress;
-			if (!item.finished) done = false;
-		}
+        for (const item of this.loadingTracker) {
+            total++;
+            finished += item.progress;
+            if (!item.finished) done = false;
+        }
 
-		return (finished / total) * 100;
-	}
+        return (finished / total) * 100;
+    }
 
-	private isLoadingDone(): boolean
-	{
-		for (const entry of this.loadingTracker) {
-			if (!entry.finished) return false;
-		}
-		return true;
-	}
+    private isLoadingDone(): boolean {
+        for (const entry of this.loadingTracker) {
+            if (!entry.finished) return false;
+        }
+        return true;
+    }
 }
