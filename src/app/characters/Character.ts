8c1a19989096b0db6797594a66f3d441699169eb
ianisvincent <ianis.vincent@twelv.fr>
2023-10-29 16:01:45 +0100

    [Feat] Refactor, add CharacterPhysics class

--- src/app/characters/Character.ts
+++ src/app/characters/Character.ts
@@ -30,6 +30,7 @@ import { WeaponType } from '../weapons/weapon-type';
 import * as GUI from '../../lib/utils/dat.gui';
 import { VehicleInteraction } from './VehicleInteraction';
 import { CharacterControls } from './CharacterControls';
+import { CharacterPhysics } from './CharacterPhysics';
 
 export class Character extends THREE.Object3D implements IWorldEntity, IDamageable, IDieable {
   public updateOrder = 1;
@@ -96,7 +97,6 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
   public vehicleEntryInstance: VehicleEntryInstance = null;
 
 
-  private physicsEnabled = true;
 
   public isAiming = false;
   public health = 100;
@@ -108,12 +108,14 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
 
   private vehicleInteraction: VehicleInteraction;
   private characterControls: CharacterControls;
+  private characterPhysics: CharacterPhysics;
 
   constructor(gltf: any, public characterService?: CharacterService) {
     super();
     this.readCharacterData(gltf);
     this.vehicleInteraction = new VehicleInteraction(this);
     this.characterControls = new CharacterControls(this);
+    this.characterPhysics = new CharacterPhysics(this);
     this.setAnimations(gltf.animations);
 
     // The visuals group is centered for easy character tilting
@@ -228,7 +230,7 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
   public onDie(): void {
     this.isDead = true;
   }
-
+  // Character weapon interaction
   public setRightHand(): void {
     this.children.forEach(children => {
       this.rightHand = children.getObjectByName(BodyPart.RightHand);
@@ -258,7 +260,7 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
 
     this.rightHand.add(mesh);*/
   }
-
+  // Character animation
   public setAnimations(animations: []): void {
     this.animations = animations;
   }
@@ -281,7 +283,7 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
   }
 
   public setPosition(x: number, y: number, z: number): void {
-    if (this.physicsEnabled) {
+    if (this.characterPhysics.physicsEnabled) {
       this.characterCapsule.body.previousPosition = new CANNON.Vec3(x, y, z);
       this.characterCapsule.body.position = new CANNON.Vec3(x, y, z);
       this.characterCapsule.body.interpolatedPosition = new CANNON.Vec3(x, y, z);
@@ -328,15 +330,9 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
     behaviour.character = this;
     this.behaviour = behaviour;
   }
-
+  // Character physics
   public setPhysicsEnabled(value: boolean): void {
-    this.physicsEnabled = value;
-
-    if (value === true) {
-      this.world.physicsWorld.addBody(this.characterCapsule.body);
-    } else {
-      this.world.physicsWorld.remove(this.characterCapsule.body);
-    }
+    this.characterPhysics.setPhysicsEnabled(value);
   }
 
   public readCharacterData(gltf: any): void {
@@ -391,13 +387,13 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
     this.vehicleEntryInstance?.update(timeStep);
     this.charState?.update(timeStep);
     // this.visuals.position.copy(this.modelOffset);
-    if (this.physicsEnabled) { this.springMovement(timeStep); }
-    if (this.physicsEnabled) { this.springRotation(timeStep); }
-    if (this.physicsEnabled) { this.rotateModel(); }
+    if (this.characterPhysics.physicsEnabled) { this.springMovement(timeStep); }
+    if (this.characterPhysics.physicsEnabled) { this.springRotation(timeStep); }
+    if (this.characterPhysics.physicsEnabled) { this.rotateModel(); }
     if (this.mixer !== undefined) { this.mixer.update(timeStep); }
 
     // Sync physics/graphics
-    if (this.physicsEnabled) {
+    if (this.characterPhysics.physicsEnabled) {
       this.position.set(
         this.characterCapsule.body.interpolatedPosition.x,
         this.characterCapsule.body.interpolatedPosition.y,
@@ -431,7 +427,7 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
   public inputReceiverUpdate(timeStep: number): void {
     this.characterControls.inputReceiverUpdate(timeStep);
   }
-
+  // Character animation
   public setAnimation(clipName: string, fadeIn: number, runOnlyOnce?: boolean, lockWhenFinished?: boolean): number {
     if (this.mixer !== undefined) {
       // gltf
@@ -454,7 +450,7 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
       return action.getClip().duration;
     }
   }
-
+  // Character animation
   public updateAimAnimation(clipName: string, cameraRotation, vector): void {
     if (this.mixer !== undefined) {
       this.clip = THREE.AnimationClip.findByName(this.animations, clipName);
@@ -502,14 +498,14 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
 
     return new THREE.Vector3(positiveX + negativeX, 0, positiveZ + negativeZ).normalize();
   }
-
+  // Character camera
   public getCameraRelativeMovementVector(): THREE.Vector3 {
     const localDirection = this.getLocalMovementDirection();
     const flatViewVector = new THREE.Vector3(this.viewVector.x, 0, this.viewVector.z).normalize();
 
     return Utils.appplyVectorMatrixXZ(flatViewVector, localDirection);
   }
-
+  // Character camera
   public setCameraRelativeOrientationTarget(): void {
     if (this.vehicleEntryInstance === null) {
       const moveVector = this.getCameraRelativeMovementVector();
@@ -570,139 +566,15 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
   }
 
   public physicsPreStep(body: CANNON.Body, character: Character): void {
-    character.feetRaycast();
-
-    // Raycast debug
-    if (character.rayHasHit) {
-      if (character.raycastBox.visible) {
-        character.raycastBox.position.x = character.rayResult.hitPointWorld.x;
-        character.raycastBox.position.y = character.rayResult.hitPointWorld.y;
-        character.raycastBox.position.z = character.rayResult.hitPointWorld.z;
-      }
-    } else {
-      if (character.raycastBox.visible) {
-        character.raycastBox.position.set(body.position.x, body.position.y - character.rayCastLength -
-            character.raySafeOffset, body.position.z);
-      }
-    }
+    this.characterPhysics.physicsPreStep(body, character);
   }
 
   public feetRaycast(): void {
-    // Player ray casting
-    // Create ray
-    const body = this.characterCapsule.body;
-    const start = new CANNON.Vec3(body.position.x, body.position.y, body.position.z);
-    const end = new CANNON.Vec3(body.position.x, body.position.y - this.rayCastLength - this.raySafeOffset, body.position.z);
-    // Raycast options
-    const rayCastOptions = {
-      collisionFilterMask: CollisionGroups.Default,
-      skipBackfaces: true      /* ignore back faces */
-    };
-    // Cast the ray
-    this.rayHasHit = this.world.physicsWorld.raycastClosest(start, end, rayCastOptions, this.rayResult);
+    this.characterPhysics.feetRaycast();
   }
 
   public physicsPostStep(body: CANNON.Body, character: Character): void {
-    // Get velocities
-    const simulatedVelocity = new THREE.Vector3(body.velocity.x, body.velocity.y, body.velocity.z);
-    // Take local velocity
-    let arcadeVelocity = new THREE.Vector3().copy(character.velocity).multiplyScalar(character.moveSpeed);
-    // Turn local into global
-    arcadeVelocity = Utils.appplyVectorMatrixXZ(character.orientation, arcadeVelocity);
-
-    let newVelocity = new THREE.Vector3();
-
-    // Additive velocity mode
-    if (character.arcadeVelocityIsAdditive) {
-      newVelocity.copy(simulatedVelocity);
-
-      const globalVelocityTarget = Utils.appplyVectorMatrixXZ(character.orientation, character.velocityTarget);
-      const add = new THREE.Vector3().copy(arcadeVelocity).multiply(character.arcadeVelocityInfluence);
-
-      if (Math.abs(simulatedVelocity.x) < Math.abs(globalVelocityTarget.x * character.moveSpeed)
-          || Utils.haveDifferentSigns(simulatedVelocity.x, arcadeVelocity.x)) {
-        newVelocity.x += add.x;
-      }
-      if (Math.abs(simulatedVelocity.y) < Math.abs(globalVelocityTarget.y * character.moveSpeed)
-          || Utils.haveDifferentSigns(simulatedVelocity.y, arcadeVelocity.y)) {
-        newVelocity.y += add.y;
-      }
-      if (Math.abs(simulatedVelocity.z) < Math.abs(globalVelocityTarget.z * character.moveSpeed)
-          || Utils.haveDifferentSigns(simulatedVelocity.z, arcadeVelocity.z)) {
-        newVelocity.z += add.z;
-      }
-    } else {
-      newVelocity = new THREE.Vector3(
-        THREE.MathUtils.lerp(simulatedVelocity.x, arcadeVelocity.x, character.arcadeVelocityInfluence.x),
-        THREE.MathUtils.lerp(simulatedVelocity.y, arcadeVelocity.y, character.arcadeVelocityInfluence.y),
-        THREE.MathUtils.lerp(simulatedVelocity.z, arcadeVelocity.z, character.arcadeVelocityInfluence.z),
-      );
-    }
-    // If we're hitting the ground, stick to ground
-    if (character.rayHasHit) {
-      // Flatten velocity
-      newVelocity.y = 0;
-
-      // Move on top of moving objects
-      if (character.rayResult.body.mass > 0) {
-        const pointVelocity = new CANNON.Vec3();
-        character.rayResult.body.getVelocityAtWorldPoint(character.rayResult.hitPointWorld, pointVelocity);
-        newVelocity.add(Utils.threeVector(pointVelocity));
-      }
-
-      // Measure the normal vector offset from direct "up" vector
-      // and transform it into a matrix
-      const up = new THREE.Vector3(0, 1, 0);
-      const normal = new THREE.Vector3(character.rayResult.hitNormalWorld.x, character.rayResult.hitNormalWorld.y,
-          character.rayResult.hitNormalWorld.z);
-      const q = new THREE.Quaternion().setFromUnitVectors(up, normal);
-      const m = new THREE.Matrix4().makeRotationFromQuaternion(q);
-
-      // Rotate the velocity vector
-      newVelocity.applyMatrix4(m);
-
-      // Compensate for gravity
-      // newVelocity.y -= body.world.physicsWorld.gravity.y / body.character.world.physicsFrameRate;
-
-      // Apply velocity
-      body.velocity.x = newVelocity.x;
-      body.velocity.y = newVelocity.y;
-      body.velocity.z = newVelocity.z;
-      // Ground character
-      body.position.y = character.rayResult.hitPointWorld.y + character.rayCastLength + (newVelocity.y / character.world.physicsFrameRate);
-    } else {
-      // If we're in air
-      body.velocity.x = newVelocity.x;
-      body.velocity.y = newVelocity.y;
-      body.velocity.z = newVelocity.z;
-
-      // Save last in-air information
-      character.groundImpactData.velocity.x = body.velocity.x;
-      character.groundImpactData.velocity.y = body.velocity.y;
-      character.groundImpactData.velocity.z = body.velocity.z;
-    }
-    // Jumping
-    if (character.wantsToJump) {
-      // If initJumpSpeed is set
-      if (character.initJumpSpeed > -1) {
-        // Flatten velocity
-        body.velocity.y = 0;
-        const speed = Math.max(character.velocitySimulator.position.length() * 4, character.initJumpSpeed);
-        body.velocity = Utils.cannonVector(character.orientation.clone().multiplyScalar(speed));
-      } else {
-        // Moving objects compensation
-        const add = new CANNON.Vec3();
-        character.rayResult.body.getVelocityAtWorldPoint(character.rayResult.hitPointWorld, add);
-        body.velocity.vsub(add, body.velocity);
-      }
-
-      // Add positive vertical velocity
-      body.velocity.y += 4;
-      // Move above ground by 2x safe offset value
-      body.position.y += character.raySafeOffset * 2;
-      // Reset flag
-      character.wantsToJump = false;
-    }
+    this.characterPhysics.physicsPostStep(body, character);
   }
 
   public addToWorld(world: World): void {
@@ -751,7 +623,7 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
     }
   }
 
-  // TODO: make something cleaner
+  // Character weapon interaction
   public loadWeapon(weaponType: WeaponType): void {
     const loader = new GLTFLoader();
     const rightHand = this.getObjectByName(BodyPart.RightHand);
@@ -775,19 +647,19 @@ export class Character extends THREE.Object3D implements IWorldEntity, IDamageab
     });
     this.hasWeaponLoaded = true;
   }
-
+  // Character weapon interaction
   public unloadWeapon(): void {
     const rightHand = this.getObjectByName(BodyPart.RightHand);
     const gun = this.getObjectByName('gun');
     rightHand.remove(gun);
     this.hasWeaponLoaded = false;
   }
-
+  // Character weapon interaction
   lockAiming(character: Character): void {
     this.world.cameraOperator.aimingMode = true;
     this.world.cameraOperator.targetedCharacter = character;
   }
-
+  // Character weapon interaction
   unlockAiming(): void {
     this.world.cameraOperator.aimingMode = false;
   }
